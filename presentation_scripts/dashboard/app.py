e4b7305ab8ee5bba653aa8a24f36d68678e500ad
likethebourbon <williams.evanpaul@gmail.com>
2021-06-15 14:09:47 -0400

    Add app code

--- presentation_scripts/dashboard/app.py
+++ presentation_scripts/dashboard/app.py
@@ -1,2 +1,13 @@
 """Code to instantiate the web app. Import the app from this file into other
 files rather than making a new instance."""
+
+import dash
+import dash_bootstrap_components as dbc
+
+# Initialize app
+app = dash.Dash(
+    __name__,
+    external_stylesheets=[dbc.themes.LITERA],
+)
+server = app.server
+app.config.suppress_callback_exceptions = True
